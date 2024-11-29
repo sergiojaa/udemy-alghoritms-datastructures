2013db8e14156d22450398c4d9f1b0c4e018e17a
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-29 12:30:46 +0400

    Add files via upload

--- classes.js
+++ classes.js
@@ -1,19 +1,41 @@
-
-class Rectangle {
-    constructor (_width, _height, _color) {
-      console.log('the rectangle is created')
+// class Rectangle {
+//     constructor (_width, _height, _color) {
+//       console.log('the rectangle is created')
   
-      this.width = _width
-    this.height = _height
-    this.color = _color
-    }
+//       this.width = _width
+//     this.height = _height
+//     this.color = _color
+//     }
   
-    getArea (){
-      return this.width * this.height
-    }
+//     getArea (){
+//       return this.width * this.height
+//     }
+//     printDescription(){
+//         console.log(`I am a rectangle of ${this.width} * ${this.height} and I am ${this.color}`)
+//     }
     
+//   }
+//   let myRectangle1 = new Rectangle(3,5,"blue");
+//   let myRectangle2 = new Rectangle(10,5,"red");
+//   myRectangle1.printDescription()
+
+  //getters setters gamoikeneba:
+  //განსაზღვრეთ მეთოდები კლასზე, რომლებიც შემდეგ გამოიყენება, 
+  //თითქოს ისინი თვისებებია, ისინი ჰგვანან თვისებებს, მაგრამ 
+  //ისინი კლასის მეთოდებია
+
+  class Square {
+    constructor (_width){
+        this.width = _width 
+        this.height = _width 
+    }
+
+    get area (){
+        return this.width + this.height;
+    }
   }
-  let myRectangle1 = new Rectangle(3,5,"blue");
-  let myRectangle2 = new Rectangle(10,5,"red");
+  let square1 = new Square(25)
+  console.log(square1.area)    // () agaraa sachirp
   
-  console.log(myRectangle2.getArea())
+
+
