3e7db5579b957e342890e1fea1d2b5668c584a09
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-29 20:26:01 +0400

    Add files via upload

--- Big-O.js
+++ Big-O.js
@@ -158,9 +158,26 @@ class LinkedList{
     this.tail = this.head;
     this.length = 1
   }
+  push(value){
+    const newNode = new Node(value)
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode
+    } else{
+      this.tail.next = newNode;
+      this.tail = newNode
+    }
+    this.length++;
+    return this
+  }
 }
-let myLinkedList = new LinkedList(4)
+let myLinkedList = new LinkedList(1)
 console.log(myLinkedList)
+console.log(myLinkedList.push(2))
+console.log(myLinkedList.push(3))
+
+
+
 // const newNode = new Node(4)
 // console.log(newNode)
 
