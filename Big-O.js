53ec92710009499c314b6f775ac102db4aad9490
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-11-30 22:01:20 +0400

    Add files via upload

--- Big-O.js
+++ Big-O.js
@@ -145,38 +145,6 @@
 
 //big-O
 //constructor 
-class Node {
-  constructor(value){
-    this.value = value;
-    this.next = null
-  }
-}
-class LinkedList {
-  constructor(value){
-    const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = this.head;
-    this.length = 1
-
-  }
-   push(value) {
-    const newNode = new Node(value)
-      
-      if(!this.head){
-        this.head = newNode;
-        this.tail = newNode
-      }else{
-        this.tail.next = newNode;
-        this.tail = newNode
-      }
-      this.length++;
-      return this
-    }   
-  }
-let myLinkedList = new LinkedList(1);
-console.log(myLinkedList)
-console.log(myLinkedList.push(2))
-
 
 
 
@@ -252,4 +220,82 @@ console.log(myLinkedList.push(2))
 // let myLinkedList = new LinkedLists(7); // Use the correct class name
 // myLinkedList.push(4);
 
+// console.log(myLinkedList);
+
+//pop 
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedLists {
+//   constructor(value) {
+//     const newNode = new Node(value);
+//     this.head = newNode;
+//     this.tail = newNode;
+//     this.length = 1;
+//   }
+
+//   push(value) {
+//     const newNode = new Node(value);
+
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+
+//     this.length++;
+//     return this;
+//   }
+//   pop() {
+//     if(!this.head) return undefined  //tu item ar gvaqvs
+//     let temp =this.head;     //erti elementi
+//     let pre = this.head;  //roca gvaq
+//     while(temp.next){  //ori an meti element
+//       pre = temp;
+//       temp = temp.next;
+//     }
+//     this.tail = pre;
+//     this.tail.next = null;
+//     this.length--
+//     if(this.length === 0){
+//       this.head = null;
+//       this.tail = null
+//     }
+//     return temp
+//   } 
+// }
+// pop(){
+//   if(!this.head) return undefined;
+//   let temp = this.head;
+//   let pre = this.head;
+//   while(temp.next){
+//     pre = temp;
+//     temp = temp.next
+//   }
+//   this.tail = pre;
+//   this.tail.next = null;
+//   this.length--
+//   if(this.length === 0){
+//     this.head = null;
+//     this.tail = null
+//   }
+//   return temp
+// }
+
+
+// let myLinkedList = new LinkedLists(1); // Use the correct class name
+// myLinkedList.push(2);
+// myLinkedList.push(3);
+
+// myLinkedList.push(4);
+
+
+
+// myLinkedList.pop()
 // console.log(myLinkedList);
