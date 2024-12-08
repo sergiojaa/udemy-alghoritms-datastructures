52984a7ef59001d2980b3eec11eac709302256f9
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-08 19:03:10 +0400

    Add files via upload

--- summarize.js
+++ summarize.js
@@ -64,70 +64,122 @@
 
 
 
+// class Node {
+//     constructor(name,year){
+//         this.name = name;
+//         this.year = year
+//         this.next = null
+//     }
+   
+// }
+// class LinkedList{
+//     constructor(name,year){
+//         const newNode = new Node(name,year)
+//         this.head = newNode;
+//         this.tail = this.head;
+//         this.length = 1
+//     }
+//     Push(name,year){
+//         const newNode = new Node(name,year)
+
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = newNode;
+//         }else{
+//             this.tail.next = newNode;
+//             this.tail = newNode
+//         }
+//         this.length++;
+//         return this
+//     }
+//     Pop(){
+//         if(!this.head)return undefined 
+//         let temp = this.head;
+//         let pre = this.head;
+//         while(temp.next){
+//             pre = temp;
+//             temp = temp.next
+//         }
+//         this.tail = pre;
+//         this.tail.next = null
+//         this.length --
+//         if(this.length === 0){
+//             this.head = null;
+//             this.tail = null
+//         }
+//         return temp
+
+//     }
+//     toObject() {
+//         const nodes = [];
+//         let current = this.head;
+
+//         while (current) {
+//             nodes.push({ name: current.name, year: current.year });
+//             current = current.next;
+//         }
+
+//         return nodes;
+//     }
+   
+// }
+
+// const myLinkedList = new LinkedList('ford',2022)
+// myLinkedList.Push('bmw',2022)
+// myLinkedList.Pop()
+
+// console.log(myLinkedList.toObject())
+
 class Node {
     constructor(name,year){
         this.name = name;
-        this.year = year
+        this.year = year;
         this.next = null
     }
-   
 }
-class LinkedList{
+class LinkedList {
     constructor(name,year){
         const newNode = new Node(name,year)
         this.head = newNode;
-        this.tail = this.head;
-        this.length = 1
+        this.tail = newNode;
+    }
+    toObject(){
+        const nodes = []
+        let current = this.head;
+        while(current){
+            nodes.push({name: current.name, year: current.year})
+            current = current.next
+        }
+        return nodes
     }
     Push(name,year){
         const newNode = new Node(name,year)
-
         if(!this.head){
             this.head = newNode;
             this.tail = newNode;
         }else{
             this.tail.next = newNode;
-            this.tail = newNode
+            this.tail = newNode;
         }
         this.length++;
         return this
     }
-    Pop(){
-        if(this.length ===0)return undefined 
-        let temp = this.head;
-        let pre = this.head;
-        while(temp.next){
-            pre = temp;
-            temp = temp.next
+    unShift(name,year){
+        const newNode = new Node(name,year)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
         }
-        this.tail = pre;
-        this.tail.next = null
-        this.length --
-        if(this.length === 0){
-            this.head = null;
-            this.tail = null
-        }
-        return temp
-
+        this.length++;
+        return this
     }
-    toObject() {
-        const nodes = [];
-        let current = this.head;
 
-        while (current) {
-            nodes.push({ name: current.name, year: current.year });
-            current = current.next;
-        }
-
-        return nodes;
-    }
-   
 }
-
-const myLinkedList = new LinkedList('ford',2022)
-myLinkedList.Push('bmw',2022)
-myLinkedList.Pop()
-
+const myLinkedList = new LinkedList('bmw',2022)
+myLinkedList.Push('mercedes','2024')
+myLinkedList.unShift('audi',2022)
 console.log(myLinkedList.toObject())
-
 
