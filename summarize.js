4a885e6ee6b7820ebeb8ef5f447891cad6c0454f
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-11 15:59:59 +0400

    Add files via upload

--- summarize.js
+++ summarize.js
@@ -303,6 +303,20 @@ Insert(index,value){
         this.length--;
         return temp;
     }
+    reverse(){
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
+        let prev = null
+        for(let i = 0;i<this.length;i++){
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return this;
+    }
 }
 const myLinkedList = new LinkedList('bmw',2022)
 myLinkedList.Push('mercedes','2024')
