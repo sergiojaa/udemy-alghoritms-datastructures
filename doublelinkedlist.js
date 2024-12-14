5c80cd9154c4c8ee9958210effb64f7fb36eb5ff
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-15 02:14:44 +0400

    Add files via upload

--- doublelinkedlist.js
+++ doublelinkedlist.js
@@ -19,6 +19,40 @@
 // console.log(myDoublyLinkedList);
 //push douleLinkedList 
 //1 xazi emateba newNode.prev = this.tail
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//         this.prev= null
+//     }
+// }
+
+// class DoublyLinkedList{
+//     constructor(value){
+//         const newNode = new Node(value);
+//         this.head = newNode;
+//         this.tail = this.head;
+//         this.length = 1
+//     }
+//     Push(value){
+//         const newNode = new Node(value);
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = this.head
+//         }else{
+//             this.tail.next = newNode;
+//             newNode.prev = this.tail
+//             this.tail = newNode;
+//         }
+//         this.length++
+//         return this
+//     }
+// }
+// let myDoublyLinkedList = new DoublyLinkedList(1)
+// myDoublyLinkedList.Push(2)
+// console.log(myDoublyLinkedList)
+
+// pop doublylinkedlist
 class Node{
     constructor(value){
         this.value = value;
@@ -47,7 +81,22 @@ class DoublyLinkedList{
         this.length++
         return this
     }
+    pop(){
+        if(this.length === 0) return undefined;
+        let temp = this.tail;
+        this.tail = this.tail.prev;
+        this.tail.next = null;
+        temp.prev = null;
+        this.length--
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null
+        }
+        return temp
+    }
 }
 let myDoublyLinkedList = new DoublyLinkedList(1)
 myDoublyLinkedList.Push(2)
+myDoublyLinkedList.pop()
 console.log(myDoublyLinkedList)
+
