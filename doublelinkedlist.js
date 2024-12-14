ccee51679489b77ddb81406f2321f587a293bcea
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-15 02:50:54 +0400

    Add files via upload

--- doublelinkedlist.js
+++ doublelinkedlist.js
@@ -150,3 +150,50 @@
 // console.log(myDoublyLinkedList)
 
 // shift doublylinkedlist
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev= null
+    }
+}
+
+class DoublyLinkedList{
+    constructor(value){
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1
+    }
+    Push(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail
+            this.tail = newNode;
+        }
+        this.length++
+        return this
+    }
+    shift(){
+        if(this.length === 0) return undefined;
+        let temp = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null
+        }else{
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null
+        }
+        this.length --
+        return temp
+    }
+}
+    let myDoublyLinkedList = new DoublyLinkedList(1)
+myDoublyLinkedList.Push(2)
+myDoublyLinkedList.shift()
+console.log(myDoublyLinkedList)
