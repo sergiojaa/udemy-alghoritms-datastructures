81abc849cc525af88dbb6653825d679975daf23a
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-16 00:31:01 +0400

    Add files via upload

--- doublelinkedlist.js
+++ doublelinkedlist.js
@@ -198,56 +198,56 @@
 // myDoublyLinkedList.shift()
 // console.log(myDoublyLinkedList)
 // // GET DOUBLYLinkedList
-class Node{
-    constructor(value){
-        this.value = value;
-        this.next = null;
-        this.prev= null
-    }
-}
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//         this.prev= null
+//     }
+// }
 
-class DoublyLinkedList{
-    constructor(value){
-        const newNode = new Node(value);
-        this.head = newNode;
-        this.tail = this.head;
-        this.length = 1
-    }
-    Push(value){
-        const newNode = new Node(value);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head
-        }else{
-            this.tail.next = newNode;
-            newNode.prev = this.tail
-            this.tail = newNode;
-        }
-        this.length++
-        return this
-    }
-    get(index){
-        if(index<0 || index> this.length){
-            return undefined;
-        }
-        let temp = this.head;
-        if(index<this.length/2){
-        for(let i = 0;i<index;i++){
-            temp = temp.next;
-        }
-    }else{
-        temp = this.tail;
-        for(let i = this.length-1; i>index;i--){
-            temp = temp.prev;
-        }
-    }
-        return temp
-    }
-}
-let myDoublyLinkedList = new DoublyLinkedList(1)
-myDoublyLinkedList.Push(2)
-myDoublyLinkedList.get(1)
-console.log(myDoublyLinkedList)
+// class DoublyLinkedList{
+//     constructor(value){
+//         const newNode = new Node(value);
+//         this.head = newNode;
+//         this.tail = this.head;
+//         this.length = 1
+//     }
+//     Push(value){
+//         const newNode = new Node(value);
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = this.head
+//         }else{
+//             this.tail.next = newNode;
+//             newNode.prev = this.tail
+//             this.tail = newNode;
+//         }
+//         this.length++
+//         return this
+//     }
+//     get(index){
+//         if(index<0 || index> this.length){
+//             return undefined;
+//         }
+//         let temp = this.head;
+//         if(index<this.length/2){
+//         for(let i = 0;i<index;i++){
+//             temp = temp.next;
+//         }
+//     }else{
+//         temp = this.tail;
+//         for(let i = this.length-1; i>index;i--){
+//             temp = temp.prev;
+//         }
+//     }
+//         return temp
+//     }
+// }
+// let myDoublyLinkedList = new DoublyLinkedList(1)
+// myDoublyLinkedList.Push(2)
+// myDoublyLinkedList.get(1)
+// console.log(myDoublyLinkedList)
 
 // set doublyLinkedList
 // set(index,value){
@@ -260,3 +260,29 @@ console.log(myDoublyLinkedList)
 // }
 
 // insert doubylinked list
+// insert(index,value{
+//     if(index === 0) return this.unshift.(value);
+//     if(index === this.length)return this.push(value);
+//     if(index <0 || index> this.length)return false;
+//     const newNode = new Node(value)
+//     const before = this.get(index-1)
+//     const after = before.next;
+//     before.next = newNode;
+//     newNode.prev = before;
+//     newNode.next = after;
+//     after.prev = newNode;
+//     this.length++;
+//     return true;
+
+// })
+remove(index){
+    if(index === 0 ) return this.pop()
+    if(index <0 || inde >= this.length) return undefined;
+    const temp = this.get(index)
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+    this.length--;
+    return temp
+}
