f6cd9ae62530c9cf8d6c29e7be4c8ee13eb4e408
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-15 17:27:49 +0400

    Add files via upload

--- doublelinkedlist.js
+++ doublelinkedlist.js
@@ -150,6 +150,54 @@
 // console.log(myDoublyLinkedList)
 
 // shift doublylinkedlist
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null;
+//         this.prev= null
+//     }
+// }
+
+// class DoublyLinkedList{
+//     constructor(value){
+//         const newNode = new Node(value);
+//         this.head = newNode;
+//         this.tail = this.head;
+//         this.length = 1
+//     }
+//     Push(value){
+//         const newNode = new Node(value);
+//         if(!this.head){
+//             this.head = newNode;
+//             this.tail = this.head
+//         }else{
+//             this.tail.next = newNode;
+//             newNode.prev = this.tail
+//             this.tail = newNode;
+//         }
+//         this.length++
+//         return this
+//     }
+//     shift(){
+//         if(this.length === 0) return undefined;
+//         let temp = this.head;
+//         if(this.length === 1){
+//             this.head = null;
+//             this.tail = null
+//         }else{
+//             this.head = this.head.next;
+//             this.head.prev = null;
+//             temp.next = null
+//         }
+//         this.length --
+//         return temp
+//     }
+// }
+//     let myDoublyLinkedList = new DoublyLinkedList(1)
+// myDoublyLinkedList.Push(2)
+// myDoublyLinkedList.shift()
+// console.log(myDoublyLinkedList)
+// // GET DOUBLYLinkedList
 class Node{
     constructor(value){
         this.value = value;
@@ -178,22 +226,25 @@ class DoublyLinkedList{
         this.length++
         return this
     }
-    shift(){
-        if(this.length === 0) return undefined;
-        let temp = this.head;
-        if(this.length === 1){
-            this.head = null;
-            this.tail = null
-        }else{
-            this.head = this.head.next;
-            this.head.prev = null;
-            temp.next = null
+    get(index){
+        if(index<0 || index> this.length){
+            return undefined;
         }
-        this.length --
+        let temp = this.head;
+        if(index<this.length/2){
+        for(let i = 0;i<index;i++){
+            temp = temp.next;
+        }
+    }else{
+        temp = this.tail;
+        for(let i = this.length-1; i>index;i--){
+            temp = temp.prev;
+        }
+    }
         return temp
     }
 }
-    let myDoublyLinkedList = new DoublyLinkedList(1)
+let myDoublyLinkedList = new DoublyLinkedList(1)
 myDoublyLinkedList.Push(2)
-myDoublyLinkedList.shift()
+myDoublyLinkedList.get(1)
 console.log(myDoublyLinkedList)
