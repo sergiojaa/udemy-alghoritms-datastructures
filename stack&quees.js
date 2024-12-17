ca78bc1bc3e990e4df14474ddd7ab5c097876955
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-18 01:29:40 +0400

    Add files via upload

--- stack&quees.js
+++ stack&quees.js
@@ -1,2 +1,16 @@
 // stacks & queues intro
-// stackc intro
+// stackc intro 
+// ewkoba ertmanents elementebi,mxolod zeda itemic wahla shegvidzlia
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+class Stack {
+    constructor(value){
+        const newNode = new Node(value)
+        this.top = newNode;
+        this.length = 1;
+    }
+}
