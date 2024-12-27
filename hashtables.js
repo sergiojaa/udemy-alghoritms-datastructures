22a301af4acc53bc44d97a57823c2dfa0894a5f3
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-12-27 15:59:01 +0400

    Add files via upload

--- hashtables.js
+++ hashtables.js
@@ -1,4 +1,15 @@
-console.log("Hello, world!");
-const vari = 5
-console.log(vari)
-//                 return true;
+// hast tables consructor
+class hashTable{
+    constructor(size = 7 ){
+        this.dataMap = new Array(size);
+    }
+    _hash(key){
+        let hash = 0;
+        for(let i = 0;i<key.length;i++){
+            hash = (hash + key.charCodeAt(i)  *23) % this.dataMap.length;
+        }
+        return hash
+    }
+}
+let myHashTable = new hashTable();  
+console.log(myHashTable)
