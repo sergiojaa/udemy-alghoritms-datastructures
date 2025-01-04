b3f8428095d418a9cb3dc54641c5dc61b7a390d0
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-04 15:54:28 +0400

    Add files via upload

--- graphs.js
+++ graphs.js
@@ -1,1 +1,16 @@
-console.log('graphs')
+// graphs 
+// constructor   Add vertex 
+class Graph {
+    constructor(){
+        this.adjacencyList = []
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = [];
+            return true;
+        }
+        return false;
+    }
+}
+let myGraph = new Graph()
+console.log(myGraph)
