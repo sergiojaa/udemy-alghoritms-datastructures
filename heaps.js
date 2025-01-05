efce92a46d9aaac43da881429c565f07b0ee85e1
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-06 01:11:40 +0400

    Add files via upload

--- heaps.js
+++ heaps.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+class Heap {
+    #heap = []
+    getHeap(){
+        return [...this.#heap]
+    }
+    #leftChild(index){
+        return 2 * index + 1;
+    }
+    #rightChild(index){
+        return 2 * index + 2
+    }
+    #parent(index){
+        return Math.floor((index - 1)/2)
+    }
+    #swap(index1,index2){
+        [this.#heap[index1], this.#heap[index2]] = [this.#heap[index2], this.#heap[index1]]
+    }
+}
