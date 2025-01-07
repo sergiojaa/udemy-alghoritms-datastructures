71e4c094f6b081010c888ec40f8c41cdcaf10636
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-07 23:06:46 +0400

    Add files via upload

--- trees.js
+++ trees.js
@@ -1,4 +1,4 @@
-j// constructor trees, binary search trees.
+// constructor trees, binary search trees.
 // class Node{
 //     constructor(value){
 //         this.value = value;
@@ -14,26 +14,88 @@ j// constructor trees, binary search trees.
 // let myTree = new BST()
 // console.log(myTree)
 //BST insert   da // contains
-class Node{
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+// class BST{
+//     constructor(){
+//         this.root = null;
+//     }
+//     insert(value){
+//         const newNode = new Node(value)
+//         if (this.root === null){
+//             this.root=newNode;
+//             return newNode;
+//         }
+//         let temp = this.root;
+//         while(true){
+//             if (newNode.value === temp.value) return undefined;
+//             if(newNode.value < temp.value){
+//                 if(temp.left === null){
+//                     temp.left = newNode;
+//                     return this;
+//                 }
+//                 temp = temp.left;
+//             }else{
+//                     if(temp.right === null){
+//                         temp.right = newNode;
+//                         return this;
+//                     }
+//                     temp = temp.right;
+//             }
+            
+//         }
+//     }
+//     contains(value){
+//         if(this.root === null) return false;
+//         let temp = this.root;
+//         while(temp){
+//             if(value < temp.value){
+//                 temp = temp.left
+//             }else if(value > temp.value){
+//                 temp = temp.right;
+//             }else{
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+// let myTree = new BST()
+// myTree.insert(12)
+// myTree.insert(20)
+// myTree.insert(21)
+// myTree.insert(11)
+// myTree.insert(155)
+// myTree.insert(133)
+// myTree.insert(12)
+// console.log(myTree.contains(12))
+
+class Node {
     constructor(value){
         this.value = value;
         this.left = null;
         this.right = null;
     }
+
 }
-class BST{
+class bst{
     constructor(){
-        this.root = null;
+       this.root = null;
     }
     insert(value){
         const newNode = new Node(value)
-        if (this.root === null){
-            this.root=newNode;
-            return newNode;
+        if(this.root === null){
+            this.root = newNode;
+            return this;
         }
         let temp = this.root;
         while(true){
-            if (newNode.value === temp.value) return undefined;
+            if(newNode === temp)return undefined;
             if(newNode.value < temp.value){
                 if(temp.left === null){
                     temp.left = newNode;
@@ -41,13 +103,12 @@ class BST{
                 }
                 temp = temp.left;
             }else{
-                    if(temp.right === null){
-                        temp.right = newNode;
-                        return this;
-                    }
-                    temp = temp.right;
+                if(temp.right === null){
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
             }
-            
         }
     }
     contains(value){
@@ -55,8 +116,8 @@ class BST{
         let temp = this.root;
         while(temp){
             if(value < temp.value){
-                temp = temp.left
-            }else if(value > temp.value){
+                temp = temp.left;
+            }else if(value  > temp.value){
                 temp = temp.right;
             }else{
                 return true;
@@ -65,13 +126,13 @@ class BST{
         return false;
     }
 }
-let myTree = new BST()
-myTree.insert(12)
-myTree.insert(20)
-myTree.insert(21)
-myTree.insert(11)
-myTree.insert(155)
-myTree.insert(133)
-myTree.insert(12)
-console.log(myTree.contains(12))
-
+let myTree = new bst()
+myTree.insert(65)
+myTree.insert(22)
+myTree.insert(45)
+myTree.insert(222)
+myTree.insert(34)
+myTree.insert(23)
+myTree.insert(111)
+console.log(myTree.contains(22))
+// done trees
