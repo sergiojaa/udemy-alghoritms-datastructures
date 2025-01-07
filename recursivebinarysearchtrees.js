2ac23d144bfe9d994c8e90403c9b0f6f67e172a6
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-08 00:01:40 +0400

    Add files via upload

--- recursivebinarysearchtrees.js
+++ recursivebinarysearchtrees.js
@@ -1,1 +1,61 @@
-console.log('recursivebinarysearchtrees')
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    #rInsert(value, currentNode) {
+        if (currentNode === null) {
+            return new Node(value);
+        }
+        if (value < currentNode.value) {
+            currentNode.left = this.#rInsert(value, currentNode.left);
+        } else if (value > currentNode.value) {
+            currentNode.right = this.#rInsert(value, currentNode.right);
+        }
+        return currentNode;
+    }
+
+    insert(value) {
+        if (this.root === null) {
+            this.root = new Node(value);
+            return this;
+        }
+        this.#rInsert(value, this.root);
+        return this;
+    }
+
+    rContains(value, currentNode = this.root) {
+        if (currentNode === null) return false;
+        if (value === currentNode.value) return true;
+        if (value < currentNode.value) {
+            return this.rContains(value, currentNode.left);
+        } else {
+            return this.rContains(value, currentNode.right);
+        }
+    }
+    #deleteNode(value,currentNode){
+        
+    }
+}
+
+let myTree = new BST();
+myTree.insert(12);
+myTree.insert(20);
+myTree.insert(21);
+myTree.insert(11);
+myTree.insert(155);
+myTree.insert(133);
+myTree.insert(12);
+
+console.log(myTree.rContains(12)); 
+console.log(myTree.rContains(22)); 
+
