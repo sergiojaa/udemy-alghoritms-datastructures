42e2b4db39e1ac5f99e4201fd4395c8983ce1bf6
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-09 15:41:14 +0400

    Add files via upload

--- quicksort.js
+++ quicksort.js
@@ -1,1 +1,18 @@
-console.log('quicksort.js loaded');
+function swap(array,firstIndex,secondIndex){
+    let temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
+function pivot(array, pivotIndex = 0, endIndex = array.length - 1){
+    let swapIndex = pivotIndex;
+    for(let i = pivotIndex+1;i<=endIndex;i++){
+        if(array[i] < array[pivotIndex]){
+            swapIndex++;
+            swap(array,swapIndex,i);
+        }
+    }
+    swap(array,pivotIndex,swapIndex);
+    return swapIndex;
+}
+let myArray = [3, 6, 1, 5, 2, 4];
+console.log(pivot(myArray)); // [2, 1, 3, 5, 6, 4]
