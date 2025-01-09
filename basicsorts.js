6455b5395534b30f731b3b21a13bf2eaa560d1e1
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-09 12:50:27 +0400

    Add files via upload

--- basicsorts.js
+++ basicsorts.js
@@ -14,24 +14,37 @@
 // }
 // console.log(bubbleSort([5,3,8,4,6,3,5,6,7,86,]))
 //selection sort code
-function selectionSort(array){
-    let min;
-    for(let i = 0;i<array.length-1;i++){
-        min = i;
-        for(let j = i+1;j<array.length;j++){
-            if(array[j]<array[min]){
-                min = j;
-            }   
+// function selectionSort(array){
+//     let min;
+//     for(let i = 0;i<array.length-1;i++){
+//         min = i;
+//         for(let j = i+1;j<array.length;j++){
+//             if(array[j]<array[min]){
+//                 min = j;
+//             }   
             
             
+//         }
+//         if(min !== i){
+//             let temp  =  array[i];
+//            array[i] = array[min];
+//            array[min] = temp;
+//        }
+//     }
+//     return array;
+
+// }
+// console.log(selectionSort([5,3,8,4,6,3,5,6,7,86,]))
+//insertion sort code
+function insertionSort(array){
+    let temp
+    for(let i = 1;i<array.length;i++){
+        temp = array[i]
+        for(var j =i-1;array[j]>temp;j--){
+            array[j+1] = array[j]
         }
-        if(min !== i){
-            let temp  =  array[i];
-           array[i] = array[min];
-           array[min] = temp;
-       }
+        array[j+1] = temp;
     }
     return array;
-
 }
-console.log(selectionSort([5,3,8,4,6,3,5,6,7,86,]))
+console.log(insertionSort([5,3,8,4,6,3,5,6,7,86,]))
