bd92aab8d06f2526c6a833eb3f5d38ca314c74ce
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-09 14:49:29 +0400

    Add files via upload

--- mergesort.js
+++ mergesort.js
@@ -1,1 +1,24 @@
-console.log('merger sort')
+function merge(array1,array2){
+    let combined = []
+    let i = 0
+    let j = 0
+    while(i<array1.length && j < array2.length){
+        if(array1[i] < array2[j]){
+            combined.push(array1[i])
+            i++
+        }else{
+            combined.push(array2[j])
+            j++
+        }
+    }
+    while(i<array1.length){
+        combined.push(array1[i])
+        i++
+    }
+    while(j<array2.length){
+        combined.push(array2[j])
+        j++
+    }
+    return combined
+}
+console.log(merge([1,10,50],[2,14,99,100])) 
