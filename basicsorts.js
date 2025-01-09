4ea153c35ab5068c58d4fa736a4e267597c178f4
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-01-09 12:08:53 +0400

    Add files via upload

--- basicsorts.js
+++ basicsorts.js
@@ -1,1 +1,15 @@
-console.log('basic sorts')
+// basic sorts
+//bubble sort code
+function bubbleSort(array){
+    for(let i = array.length - 1;i>0;i--){
+        for(let j = 0;j<i;j++){
+            if(array[j]>array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+}
+console.log(bubbleSort([5,3,8,4,6,3,5,6,7,86,]))
